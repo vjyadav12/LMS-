925f76e00d35ca5d969f3d5c565dcaaa50f1c86f
vijay yadav <124396738+vjyadav12@users.noreply.github.com>
2024-11-02 12:34:09 +0530

    creating course Sclice/store

--- FrontEnd/my-app/src/Redux/Store.js
+++ FrontEnd/my-app/src/Redux/Store.js
@@ -1,10 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import AuthSlice from './Slices/AuthSlice'
+import CoursesSlice from './Slices/CoursesSlice'
 
 const Store = configureStore({
     reducer:{
         auth: AuthSlice,
-    }
+       
 })
 
 export default Store
